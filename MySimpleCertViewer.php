4afa1cb123d6da2ceee8e356d5a58205d4315bc3
Wikinaut <mail@tgries.de>
2013-03-16 20:18:14 +0100

    added info section on output page

--- MySimpleCertViewer.php
+++ MySimpleCertViewer.php
@@ -13,27 +13,27 @@
 
 # The server and port are currently hardcoded
 #
-$server = "www.google.org";
+$server = "www.cacert.org";
 $port = "443";
 
 $certFilename = tempnam( "", "server-certificate-");
-exec( "echo -n | openssl s_client -connect $server:$port 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' 1> $certFilename", $out );
+exec( "echo -n " .
+	"| openssl s_client -connect $server:$port 2>/dev/null " .
+	"| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' 1> $certFilename",
+	$out
+);
 
-function decodeCert( $cert )
- {
+function decodeCert( $cert ) {
     $cert = preg_replace( '/\-+BEGIN CERTIFICATE\-+/', '', $cert );
     $cert = preg_replace( '/\-+END CERTIFICATE\-+/', '', $cert );
-    $cert = trim( $cert );
-    $cert = str_replace( array( "\n\r", "\n", "\r" ), '', $cert );
+    $cert = str_replace( array( "\n\r", "\n", "\r" ), '', trim( $cert ) );
     return base64_decode( $cert );
  }
 
 $cert = file_get_contents( $certFilename );
-
 $decCert = decodeCert( $cert );
 
 $certArray = array();
-
 $certArray['x-server-port'] = "$server:$port";
 $certArray['x-server'] = $server;
 $certArray['x-port'] = $port;
@@ -45,8 +45,18 @@ $certArray['x-fingerprints'] = array(
 );
 
 $certArray = $certArray + openssl_x509_parse( $cert );
-
 $certArray['x-certificate-base64'] = $cert;
 
-header( "Content-Type: text/plain" );
-echo print_r( $certArray , true );
+header( "Content-Type: text/html" );
+$output = print_r( $certArray , true );
+echo <<<EOF
+<h2>MySimpleCertViewer</h2>
+<pre>
+<a href="https://github.com/Wikinaut/MySimpleCertViewer">source code on GitHub</a>
+<hr>
+Certificate data for <b><a href="https://$server:$port">https://$server:$port</a></b> (x-fields added by the viewer)
+
+$output
+</pre>
+EOF;
+
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ MySimpleCertViewer
 ==================
 
 MySimpleCertViewer - a simple server certificate viewer in PHP.
+
+#### References
+* http://stackoverflow.com/questions/6426927/php-ssl-certificate-fingerprint
+* http://kubieziel.de/blog/archives/1484-Fingerprints-von-SSL-Seiten-pruefen.html
